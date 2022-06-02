27d4eaea081ae0a150741b77dfe9dc4e078e76fa
Wenega Wama <narcissewatherloy@gmail.com>
2022-06-02 17:36:04 -0300

    New feature

---
+++ Exercicio_2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lista_1_aquecimento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = num; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
--- Lista_1_aquecimento/Program.cs
+++ Lista_1_aquecimento/Program.cs
@@ -12,33 +12,42 @@ namespace Lista_1_aquecimento
         static void Main(string[] args)
         {
             Console.WriteLine("Digite seu nome : ");
-            Console.WriteLine("Digite seu CPF : ");
+            /*Console.WriteLine("Digite seu CPF : ");
             Console.WriteLine("Digite sua data de nascimento: ");
             Console.WriteLine("Digite sua renta mensal: ");
             Console.WriteLine("Digite seu estado civil(C,S V OU D) : ");
             Console.WriteLine("Digite o número de seus dependentes: ");
-            
+            */
 
 
             string nome = Console.ReadLine();
-            string cpf = Console.ReadLine();
+            /*string cpf = Console.ReadLine();
             string dataNascimento = Console.ReadLine();
             string rendaMensal = Console.ReadLine();
             string estadoCivil = Console.ReadLine();
             string dependentes = Console.ReadLine();
-
+            */
             
 
 
             Console.WriteLine(nome);
-            Console.WriteLine(cpf);
+            /*Console.WriteLine(cpf);
             Console.WriteLine(dataNascimento);
             Console.WriteLine(rendaMensal);
             Console.WriteLine(estadoCivil);
             Console.WriteLine(dependentes);
+            */
+            //Validação do nome
+            string[] listNome = Convert.ToString(Console.ReadLine());
+            Regex rgx = new Regex(@"([A-z]+\s)+");
+            foreach(string i in listNome)
+            {
+                Console.WriteLine(i);
+            }   
+
 
             //Validação do cpf
-            try
+            /*try
             {
                 if (cpf.Length == 11)
                 {
@@ -53,11 +62,12 @@ namespace Lista_1_aquecimento
 
             //Validação da data de nascimento
 
-            if ()
+            /*if ()
             {
                 dataNascimentoFormatado = dataNascimento.ToString(dataNascimento);
             }
-
+            */
+            
 
 
             Console.ReadLine();
